d6064f7c172ebcd416304b82badfd8baa8502bf5
ElishevaShm <le7sh834@gmail.com>
2025-01-22 12:18:37 +0200

    Add empty route

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 const port = 3000;
 const API_KEY = process.env.RENDER_API_KEY;
 
+app.get('/',(req,res)=>{
+  res.send('Hello from render!');
+});
 app.get('/services', async (req, res) => {
   try {
     const response = await axios.get('https://api.render.com/v1/services', {
